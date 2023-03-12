0b53b40c9f806f2168d7a4cc30d0e3b3156090f8
Amanbel <amanuel11belachew@gmail.com>
2023-03-12 21:15:59 +0300

    AirBnb

--- models/user.py
+++ models/user.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 import datetime
-
-if __name__ == '__main__':
-    from __init__ import storage
-    from base_model import BaseModel
-else:
-    from models.base_model import BaseModel
-    from models import storage
+from models.base_model import BaseModel
+from models import storage
 
 class User(BaseModel):
     email = ""
