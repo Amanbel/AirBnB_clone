099f7a11715ad62cafbb6f80fe124c032ebbe98a
Amanbel <amanuel11belachew@gmail.com>
2023-03-12 17:17:59 +0300

    AirBnb

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,25 +6,40 @@ import datetime
 
 
 class TestBaseModel(unittest.TestCase):
+    """test class for BaseModel class
+    to test its methods"""
 
     def setUp(self):
+        """function called before every test
+        function"""
         self.inst = BaseModel()
 
     def tearDown(self):
+        """function called after every test
+        function"""
         pass
 
     def test_isBaseExist(self):
+        """function to test the initialization of
+        the BaseClass"""
         self.assertIsNotNone(self.inst)
 
     def test_baseDict(self):
+        """function to test the to_dict()
+        method inside the BaseClass the
+        returns the dictionary of the object"""
         self.assertIsInstance(self.inst.to_dict(), dict)
 
     def test_datesOfBase(self):
+        """function that tests the type of the
+        value of the created_at and updated_at values"""
         Base = self.inst.to_dict()
         self.assertIsInstance(Base['created_at'], str)
         self.assertIsInstance(Base['updated_at'], str)
 
     def test_BaseUpdate(self):
+        """function that tests the save() method if
+        it updated the updated_at attribute"""
         Base = self.inst.to_dict()
         self.inst.save()
         new_Base = self.inst.to_dict()
@@ -32,6 +47,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(new_Base['created_at'], Base['created_at'])
 
     def test_BaseAttributes(self):
+        """funciton that tests the creation of
+        the fundamental attributes in the BaseClass"""
         BaseKeys = self.inst.to_dict().keys()
         self.assertIn('id', BaseKeys)
         self.assertIn('created_at', BaseKeys)
@@ -39,6 +56,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('__class__', BaseKeys)
 
     def test_BaseKwargs(self):
+        """function that tests the initialization of
+        BaseClass with a dictionary input"""
         Base_dict = self.inst.to_dict()
         Base_dict.update({'name': "Aman", 'age': 54, '__class__': 'User'})
         Base = BaseModel(**Base_dict)
