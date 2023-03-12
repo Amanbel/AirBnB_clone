7e3077c1f28bdd7215c41214a39deeb23f1fca42
Amanbel <amanuel11belachew@gmail.com>
2023-03-12 11:59:15 +0300

    AirBnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,17 +11,32 @@ class FileStorage():
     __objects = {}
 
     def all(self):
+        """returns a dictionary of all the objects with
+        the key <class name>.id and a value of 
+        the string return of the object
+        """
         return type(self).__objects
 
     def new(self, obj):
+        """creates a new dictionary element with the
+        key as <class name>.id and the value as the
+        string return of the object
+        """
         type(self).__objects.update({'{}.\
 {}'.format(obj.__class__.__name__, obj.id): str(obj)})
 
     def save(self):
+        """saves the all dictionary created by the new method
+        in the FileStorage class to a file,currently named
+        "file.json"
+        """
         with open(type(self).__file_path, 'w') as f:
             json.dump(type(self).__objects, f)
 
     def reload(self):
+        """updates the private class attribute __object with
+        the newly added attributes in the file.json file
+        """
         try:
             with open(type(self).__file_path, 'r') as f:
                 type(self).__objects.update(json.load(f))
